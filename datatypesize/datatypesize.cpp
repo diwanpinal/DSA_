9c4b2b5a4c32e1f9203d423559bd6a792ab3e301
diwanpinal <diwanpinal@gmail.com>
2022-02-19 15:36:16 +0530

    datatype size

---
+++ datatypesize/datatypesize.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std ;
+int main(){
+ int a; //declaration
+ a=100 ; //initialisation
+ cout<<"size of int"<<sizeof(a)<<endl;
+  
+
+  float b;
+  cout<<"size of float"<<sizeof(b)<<endl;
+
+  char c;
+   cout<<"size of char"<<sizeof(c)<<endl;
+
+
+   bool d;
+    cout<<"size of bool"<<sizeof(d)<<endl;
+   
+   long long int li;
+   short int si;
+    cout<<"size of longint"<<sizeof(li)<<endl;
+   cout<<"size of shortint"<<sizeof(si)<<endl;
+
+  return 0;
+}
