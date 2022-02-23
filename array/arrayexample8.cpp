1985a45e7b8d13ee59754f46a53d6b3fe36f0adc
diwanpinal <diwanpinal@gmail.com>
2022-02-23 23:43:28 +0530

    sum_of_subarrays

---
+++ array/arrayexample8.cpp
@@ -0,0 +1,26 @@
+//subarray and subquence//
+//every subarray is a sequence but every subsequence is not a subarray//
+//sum of all subarrays//
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int arr[n];
+
+for(int i=0;i<n;i++){
+
+    cin>>arr[i];
+}
+int sum=0;
+for(int i=0;i<n;i++){
+    sum=0;
+ for(int j=i;j<n;j++){
+     sum +=arr[j];
+     cout<<arr[j]<<endl;
+ }
+
+}
+
+    return 0;
+}
