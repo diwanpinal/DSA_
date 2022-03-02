bf1c15f470e724fcad78bfc09aa8ec2716080e3f
diwanpinal <diwanpinal@gmail.com>
2022-03-02 23:50:24 +0530

    possible_subarray in an_array

--- array/arrayexample11.cpp
+++ array/arrayexample11.cpp
@@ -84,7 +84,7 @@ cout<<st<<" "<<en<<" "<<endl;
 
 
 //smallest positive missing no.//
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 int main(){
 int n;
@@ -115,4 +115,24 @@ for(int i=0;i<N;i++){
 }
 cout<<ans<<endl;
     return 0 ;
+}
+*/
+//possible subarray//
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+for(int i=0; i<n;i++){
+    for(int j=i;j<n;j++){
+        for(int k=i;k<=j;k++){
+            cout<<arr[k]<<" ";
+        }cout<<endl;
+    }
+}
+ return 0 ;
 }
