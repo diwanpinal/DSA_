a5792cb945eab91009e220cd2d2af8ed2bab0835
diwanpinal <diwanpinal@gmail.com>
2022-03-12 00:17:23 +0530

    intro_of_two_d_array

--- array/twoDarray/twoDarrayexample1.cpp
+++ array/twoDarray/twoDarrayexample1.cpp
@@ -1,13 +1,32 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+
+int arr[3][4]={{0,1,2,3},{4,5,6,7},{8,9,10,11}};
+cout<<"matrix is:"<<endl;
+for(int i=0;i<3;i++){
+    for(int j=0;j<4;j++){
+        cout<<arr[i][j]<<" ";
+    }
+    cout<<"\n";
+}
+
+return 0;
+}
+*/
+/*
 #include<iostream>
 using namespace std;
 int main(){
 int n,m;
+cin>>n>>m;
 int arr[n][m];
 for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
+       for(int j=0;j<m;j++)
+       {
         cin>>arr[i][j];
-
-    }
+        }
 }
 cout<<"matrix is:"<<endl;
 for(int i=0;i<n;i++){
@@ -18,3 +37,4 @@ for(int i=0;i<n;i++){
 }
     return 0;
 }
+/*
