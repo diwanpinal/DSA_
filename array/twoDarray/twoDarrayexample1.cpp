8081315f334c04f055619d18d5fd42bf264aa407
diwanpinal <diwanpinal@gmail.com>
2022-03-29 23:47:39 +0530

    spiral_order_traversal

--- array/twoDarray/twoDarrayexample1.cpp
+++ array/twoDarray/twoDarrayexample1.cpp
@@ -39,7 +39,7 @@ for(int i=0;i<n;i++){
 }
 */
 //searching_in_twoDarrays//
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 int main(){
 int n,m;
@@ -80,3 +80,46 @@ else{
 }
  return 0;
 } 
+*/
+//spiral_order_traversal
+#include<iostream>
+using namespace std;
+int main(){
+int n ,m;
+cin>>n>>m;
+int arr[n][m];
+for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        cin>>arr[i][j];
+    }
+
+}
+//spiral order logic//
+int row_start=0, row_end=n-1,column_start=0,column_end= m-1;
+while(row_start<=row_end && column_start<=column_end){
+
+ //row_start//
+ for(int col=column_start;col<=column_end;col++){
+    cout<<arr[row_start][col]<<" ";
+ }
+ row_start++;
+
+ //column_end//
+ for(int row=row_start;row<=row_end;row++)
+  cout<<arr[row][column_end]<<" ";
+ column_end--;
+
+  //row_end//
+  for(int col=column_end; col>=column_start;col--)
+    cout<<arr[row_end][col]<<" ";
+ row_end--;
+  //column_start//
+   for(int row=row_end;row>=row_start;row--){
+    cout<<arr[row][column_start]<<" "; 
+  }
+    column_start++; 
+  }
+
+ return 0;
+}
+
