47741faf5a73811a8adcb53ae2dff34f87f06911
diwanpinal <diwanpinal@gmail.com>
2022-04-10 00:23:29 +0530

    matrix_multiplication

--- array/twoDarray/twoDarrayexample1.cpp
+++ array/twoDarray/twoDarrayexample1.cpp
@@ -126,7 +126,7 @@ while(row_start<=row_end && column_start<=column_end){
 
 //matrices_questions//matrix_transpose(row=col)// //for printinr element above diagonal//
 
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 int main(){
 int n,m;
@@ -164,3 +164,55 @@ for(int i=0;i<3;i++){
 
 return 0;
 }
+*/
+
+//matrix multiplication//
+#include<iostream>
+using namespace std;
+int main(){
+int n1,n2,n3;
+cin>>n1>>n2>>n3;
+int m1[n1][n2];
+int m2[n1][n3];
+for(int i=0;i<n1;i++){
+    for(int j=0;j<n2;j++)
+    cin>>m1[i][j];
+}
+
+for(int i=0;i<n2;i++){
+    for(int j=0;j<n3;j++)
+    cin>>m2[i][j];
+}
+int ans[n1][n3];
+for(int i=0;i<n1;i++){
+    for(int j=0;j<n3;j++){
+        ans[i][j]= 0;
+    }
+}
+for(int i=0;i<n1;i++){
+    for(int j=0;j<n3;j++){
+        for(int k=0;k<n2;k++){
+            ans[i][j] += m1[i] [k]* m2[k][j];
+        }
+    }
+}
+
+for(int i=0;i<n1;i++){
+    for(int j=0;j<n3;j++){
+    cout<<ans[i][j]<<" ";
+    }cout<<endl;
+}
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
