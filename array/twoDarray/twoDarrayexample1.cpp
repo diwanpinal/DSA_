a946f95c15f0b65a653040e5308f10387eadf688
diwanpinal <diwanpinal@gmail.com>
2022-04-12 21:32:09 +0530

    checking_character_palindrome

--- array/twoDarray/twoDarrayexample1.cpp
+++ array/twoDarray/twoDarrayexample1.cpp
@@ -270,13 +270,37 @@ while(a[i] != '\0'){
 }
 
 *///charcter array //
-
+/*
 #include<iostream>
 using namespace std ;
 int main(){
 char a[100];
 cin>>a;
 cout<<a[4];
+    return 0;
+}
+*/
+//checking palindrome//
+#include<iostream>
+using namespace std ;
+int main(){
+int n;
+cin>>n;
+char arr[n+1];
+cin>>arr;
+bool check= true;
+for(int i=0;i<n;i++){
+    if(arr[i]!=arr[n-1-i]){
+        check=false;
+    }
+}
+if(check==true){
+    cout<<"word is palindrome"<<endl;
+
+}
+else{
+    cout<<"word is not a palindrome"<<endl;
+}
     return 0;
 }
 
