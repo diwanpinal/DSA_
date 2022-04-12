872445e516996f8e36f6ea232d093f0800cd5cec
diwanpinal <diwanpinal@gmail.com>
2022-04-12 19:06:48 +0530

    matrix_search_newtechnic

--- array/twoDarray/twoDarrayexample1.cpp
+++ array/twoDarray/twoDarrayexample1.cpp
@@ -167,6 +167,7 @@ return 0;
 */
 
 //matrix multiplication//
+/*
 #include<iostream>
 using namespace std;
 int main(){
@@ -206,8 +207,51 @@ for(int i=0;i<n1;i++){
 
     return 0;
 }
+*/
+//matrix search//
+#include<iostream>
+using namespace std;
+int main(){
+int n,m;
 
+cin>>n>>m;
+int a[n][m];
+for(int i=0; i<n; i++){
+    for(int j=0; j<m;j++)
+    cin>>a[i][j];
+}
+cout<<"the matrix is:"<<endl;
+for(int i=0; i<n; i++){
+    for(int j=0; j<m;j++){
+    cout<<a[i][j]<<" ";
+    }cout<<endl;
+}
 
+int target;
+cin>>target;
+int r=0, c=m-1;
+bool found = false;
+while(r<n , c>=0){
+    if(a[r][c]==target){
+        found = true;
+    }
+    if(a[r][c]>target){
+        c--;
+    }
+    else{
+        r++;
+    }
+}
+
+if(found){
+    cout<<"element is found";
+}
+else{
+    cout<<"element is not found";
+}
+
+   return 0;
+}
 
 
 
