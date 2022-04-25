b7b6fa1bd76c66a979a8cec71cdbf2527b6b9143
diwanpinal <diwanpinal@gmail.com>
2022-04-25 22:05:11 +0530

    string_example

---
+++ string/stringexample.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+int main(){
+string str;
+cin>>str;
+cout<<str;
+string str1(5,'d');
+cout<<str1<<endl;
+string str2="diwan"; 
+cout<< str2;
+string str3;
+getline(cin,str3);
+cout<<str3<<endl;
+    return 0;
+
+}
